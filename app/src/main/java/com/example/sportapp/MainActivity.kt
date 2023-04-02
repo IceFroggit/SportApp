c9fb31d78749a46f9d7ba56d2bb1d541121d02b3
IceFroggit <zeksengali0@gmail.com>
2023-04-02 16:28:05 +0300

    added ViewModel, Question Classes(Working basic game logic)

--- app/src/main/java/com/example/sportapp/MainActivity.kt
+++ app/src/main/java/com/example/sportapp/MainActivity.kt
@@ -1,11 +1,72 @@
 package com.example.sportapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.sportapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        parseBundle(savedInstanceState)
+
+        with(binding) {
+            trueButton.setOnClickListener {
+               checkAnswer(true)
+                score.text = "High Score: ${viewModel.getScore()}%"
+                viewModel.moveToNext()
+                updateQuestion()
+            }
+            falseButton.setOnClickListener {
+                checkAnswer(true)
+                score.text = "High Score: ${viewModel.getScore()}%"
+                viewModel.moveToNext()
+                updateQuestion()
+
+            }
+            updateQuestion()
+
+        }
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = viewModel.currentQuestionText
+        binding.questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean){
+        val correctAnswer = viewModel.currentQuestionAnswer
+        var messageResId = R.string.incorrect_toast
+        if (userAnswer == correctAnswer){
+            messageResId = R.string.correct_toast
+            viewModel.currentPoint++
+        }
+        Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show()
+    }
+    private fun parseBundle(savedInstanceState: Bundle?){
+        val currentIndex = savedInstanceState?.getInt(KEY_INDEX,0) ?: 0
+        viewModel.currentIndex = currentIndex
+        val currentPoint = savedInstanceState?.getInt(KEY_INDEX,0) ?: 0
+        viewModel.currentIndex = currentPoint
+        //todo maybe do refactor
+        binding.score.text = "High Score: ${viewModel.getScore()}%"
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_INDEX,viewModel.currentIndex)
+        outState.putInt(KEY_POINT,viewModel.currentPoint)
+    }
+    companion object{
+        const val KEY_INDEX = "index"
+        const val KEY_POINT = "point"
     }
 }
---
+++ app/src/main/java/com/example/sportapp/MainViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.sportapp
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel:ViewModel() {
+    var currentPoint = 0
+    var textScore = 0
+    var currentIndex = 0
+    private val questionBank = listOf(
+        Question(R.string.question_australia,true),
+        Question(R.string.question_oceans,true),
+        Question(R.string.question_mideast,false),
+        Question(R.string.question_africa,false),
+        Question(R.string.question_americas,true),
+        Question(R.string.question_asia,true),
+        Question(R.string.ending,true))
+    val currentQuestionAnswer:Boolean
+        get() = questionBank[currentIndex].answer
+    val currentQuestionText:Int
+        get() = questionBank[currentIndex].textResId
+
+    fun getScore()= 100*currentPoint/questionBank.size
+
+    fun moveToNext(){
+        currentIndex = (currentIndex + 1) % questionBank.size
+    }
+
+    fun updateQuestions() {
+        TODO("Not yet implemented")
+    }
+
+    fun checkAnswer(b: Boolean) {
+        TODO("Not yet implemented")
+    }
+}
